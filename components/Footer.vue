5e304f8aa04bea933a8d79311eafdb89300873a5
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-06-26 19:30:57 +0400

    Убрал баг

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,13 +2,13 @@
   <div class="md:mt-26 mt-25">
     <div class="grid grid-cols-8">
       <div class="row-start-1">
-        <a href="youtube" target="_blank">youtube</a>
-        <a href="vk" target="_blank">вконтакте</a>
         <a href="web.telegram" target="_blank">telegram</a>
+        <a href="youtube:/" target="_blank">youtube</a>
+        <a href="vk" target="_blank">вконтакте</a>
       </div>
-      <div class="row-start-1">
+      <!--<div class="row-start-1">
         <p class="xl:text-[2.75rem] md:text-[1.5rem]">У нас происходит много всего! Подпишись, чтобы быть в курсе</p>
-      </div>
+      </div>-->
     </div>
   </div>
 </template>
