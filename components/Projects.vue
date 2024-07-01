cfe7468d7b2d072a393c138a450997fcee7d6ef8
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-07-01 14:47:00 +0400

    Поправил заголовок

--- components/Projects.vue
+++ components/Projects.vue
@@ -4,7 +4,7 @@
       <div
         class="md:grid grid-cols-8 md:mb-20 mb-10 2xl:text-2xl xl:text-xl md:text-[4.5rem] text-[2.1875rem] tracking-[-0.07em] 2xl:leading-[7.3125rem] leading-[100%] ml-5 md:ml-0"
       >
-        <div class="col-start-3 col-end-9">отличны</div>
+        <div class="col-start-3 col-end-9">харизмы</div>
         <p class="ml-5 md:ml-0 col-start-4 col-end-9 text-orange">в проектах</p>
       </div>
 
