32e1e4776fd21053417b7c99ed100918cf0e10f2
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-07-05 16:05:32 +0400

    Nuxt-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,30 +7,15 @@ export default defineNuxtConfig({
         lang: "RU"
       },
       title: "Харизмы",
-      //script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }],
       link: [{ rel: 'shortcut icon', href: 'favicon.ico' }],
       meta: [{ hid: 'description', name: 'description', content: 'Харизмы' }]
     }
   },
-  builder: 'vite',
-  vite: {
-    build: {
-      chunkSizeWarningLimit: 1000
-    }
-  },
   css: ['normalize.css', 'vue-final-modal/style.css'],
-  //devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  build: {
-    transpile: ['gsap'],
-  },
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
   },
-
   ssr: false,
-  //typescript: {
-  //  typeCheck: true
-  //},
 })
