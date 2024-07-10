2ca17ebc0c3aaa3c2ec5f697c195b9c0bf8f1e36
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-07-10 15:53:23 +0400

    Поправил текст

--- components/sections/section/numbers/index.vue
+++ components/sections/section/numbers/index.vue
@@ -5,11 +5,9 @@
         <div class="number-point-1 shrink-0 max-w-[16.5rem] md:max-w-none md:w-auto w-screen">
           <div class="z-10 max-w-[27.875rem] min-w-[14rem] xl:mr-[9.375rem] md:mr-[5rem] mr-[1.6875rem]">
             <div class="md:pb-5 pb-2.5 pd:text-sm text-[0.625rem]">{ 1 }</div>
-            <div class="pb-2.5 md:text-lg text-sm uppercase font-medium">133 балла </div>
+            <div class="pb-2.5 md:text-lg text-sm uppercase font-medium">133 балла</div>
             <div class="pb-2.5 md:text-[1.25rem] text-sm font-petrov">средний IQ сотрудников компании,</div>
-            <div class="font-petrov md:text-[1.25rem] text-[0.75rem]">
-              сообразительность и креативность - наш конек
-            </div>
+            <div class="font-petrov md:text-[1.25rem] text-[0.75rem]">сообразительность и креативность - наш конек</div>
           </div>
         </div>
         <div class="number-point-1 shrink-0 max-w-[16.5rem] md:max-w-none md:w-auto w-screen">
@@ -29,11 +27,13 @@
               src="/images/points/point-3.png"
               alt="Здание"
             />
-            <div class="max-w-[26.875rem] min-w-[14rem] xl:ml-[7.5625rem] md:ml-[2.5rem] ml-[1.25rem] mb-5">
+            <div class="max-w-[28.875rem] min-w-[14rem] xl:ml-[7.5625rem] md:ml-[2.5rem] ml-[1.25rem] mb-5">
               <p class="md:mb-5 mb-2.5 md:text-sm text-[0.625rem]">{ 3 }</p>
               <p class="mb-2.5 md:text-lg text-sm uppercase font-medium">91% сотрудников</p>
-              <p class="font-petrov md:text-[1.25rem] text-[0.75rem] max-w-[25rem]">
-                в хорошем настроении и высокой энергии по матрице эмоционального состояния Д.Карузо мы бодры, веселы...
+              <p class="font-petrov md:text-[1.25rem] text-[0.75rem] max-w-[27rem]">
+                в хорошем настроении и высокой энергии по матрице эмоционального состояния Д.Карузо
+                <br class="md:block hidden" />
+                мы бодры, веселы...
               </p>
             </div>
           </div>
@@ -67,11 +67,11 @@
                 мы меняемся, не изменяя себе!
               </p>
             </div>
-            <div class="max-w-[26.875rem] min-w-[14rem] pr-5 mb-5 mt-auto">
+            <div class="max-w-[28.875rem] min-w-[14rem] pr-5 mb-5 mt-auto">
               <p class="md:mb-5 mb-2.5 md:text-sm text-[0.625rem]">{ 6 }</p>
               <p class="mb-2.5 md:text-lg text-sm uppercase font-medium">37 лет</p>
               <p class="font-petrov md:text-[1.25rem] text-[0.75rem]">
-                средний возраст соратников внутри коллектива <br class="md:block hidden" />
+                средний возраст соратников внутри коллектива, <br class="md:block hidden" />
                 все абсолютно молоды душой
               </p>
             </div>
