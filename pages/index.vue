9f1872bbb2b452402de5d7956a42c8c105e88c8e
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-07-12 17:38:27 +0400

    Вернул блоки убрал шрифты

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <!--<sections-section-hero />-->
-  <!--<sections-section-promo />-->
+  <sections-section-hero />
+  <sections-section-promo />
   <sections-section-points />
-  <!--<div class="bg-black">
+  <div class="bg-black">
     <sections-section-quality />
-  </div>-->
-  <!--<sections-section-peoples />-->
+  </div>
+  <sections-section-peoples />
   <sections-section-numbers />
   <sections-section-projects />
   <div class="bg-black">
