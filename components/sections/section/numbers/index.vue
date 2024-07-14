0a6e9404cbcc6c2128f2cebe7b61feff5658c027
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-07-14 14:38:12 +0400

    Вернул блок в цифрах

--- components/sections/section/numbers/index.vue
+++ components/sections/section/numbers/index.vue
@@ -3,7 +3,7 @@
     <div class="relative z-20 numbers flex items-center" ref="scrollNumbers">
       <div class="flex flex-nowrap md:py-25 py-20">
         <div class="number-point-1 shrink-0 max-w-[16.5rem] md:max-w-none md:w-auto w-screen">
-          <div class="z-10 max-w-[27.875rem] min-w-[14rem] xl:mr-[9.375rem] md:mr-[5rem] mr-[1.6875rem] pt-10">
+          <div class="z-10 max-w-[27.875rem] min-w-[14rem] xl:mr-[9.375rem] md:mr-[5rem] mr-[1.6875rem]">
             <div class="md:pb-5 pb-2.5 pd:text-sm text-[0.625rem]">{ 1 }</div>
             <div class="pb-2.5 md:text-lg text-sm uppercase font-medium">133 балла</div>
             <div class="pb-2.5 md:text-[1.25rem] text-sm font-petrov">средний IQ сотрудников компании,</div>
@@ -11,8 +11,8 @@
           </div>
         </div>
         <div class="number-point-1 shrink-0 max-w-[16.5rem] md:max-w-none md:w-auto w-screen">
-          <div class="z-10 xl:mr-[10rem] md:mr-[5rem] mr-[2rem]">
-            <div class="pb-2">
+          <div class="z-10 mt-20 xl:mr-[10rem] md:mr-[5rem] mr-[2rem]">
+            <div class="mb-2">
               <div class="md:pb-5 pb-2.5 pd:text-sm text-[0.625rem]">{ 2 }</div>
               <div class="pb-2.5 md:text-lg text-sm uppercase font-medium">1 000 футбольных полей</div>
               <div class="font-petrov md:text-[1.25rem] text-[0.75rem]">
@@ -21,15 +21,15 @@
               </div>
             </div>
             <img
-              class="block xl:pb-[10.25rem] md:pb-[5.625rem] pb-[3.5625rem] min-w-[13.4375rem] md:w-[27.8125rem] w-[13.4375rem]"
+              class="block xl:mb-[10.25rem] md:mb-[5.625rem] mb-[3.5625rem] min-w-[13.4375rem] md:w-[27.8125rem] w-[13.4375rem]"
               width="445"
               height="269"
               src="/images/points/point-3.png"
               alt="Здание"
             />
-            <div class="max-w-[28.875rem] min-w-[14rem] xl:ml-[7.5625rem] md:ml-[2.5rem] ml-[1.25rem] pb-5">
-              <p class="md:pb-5 pb-2.5 md:text-sm text-[0.625rem]">{ 3 }</p>
-              <p class="pb-2.5 md:text-lg text-sm uppercase font-medium">91% сотрудников</p>
+            <div class="max-w-[28.875rem] min-w-[14rem] xl:ml-[7.5625rem] md:ml-[2.5rem] ml-[1.25rem] mb-5">
+              <p class="md:mb-5 mb-2.5 md:text-sm text-[0.625rem]">{ 3 }</p>
+              <p class="mb-2.5 md:text-lg text-sm uppercase font-medium">91% сотрудников</p>
               <p class="font-petrov md:text-[1.25rem] text-[0.75rem] max-w-[27rem]">
                 в хорошем настроении и высокой энергии по матрице эмоционального состояния Д.Карузо
                 <br class="md:block hidden" />
@@ -40,16 +40,16 @@
         </div>
         <div class="number-point-1 shrink-0 max-w-[16.5rem] md:max-w-none md:w-auto w-screen">
           <div class="z-10">
-            <div class="max-w-[26.875rem] min-w-[14rem] pt-10">
-              <p class="md:pb-5 pb-2.5 md:text-sm text-[0.625rem]">{ 4 }</p>
-              <p class="pb-2.5 md:text-lg text-sm uppercase font-medium">516 лет</p>
+            <div class="max-w-[26.875rem] min-w-[14rem]">
+              <p class="md:mb-5 mb-2.5 md:text-sm text-[0.625rem]">{ 4 }</p>
+              <p class="mb-2.5 md:text-lg text-sm uppercase font-medium">516 лет</p>
               <p class="font-petrov md:text-[1.25rem] text-[0.75rem]">
                 разницы между открытием Америки и нашим <br />
                 но у нас тоже амбициозные планы
               </p>
             </div>
             <img
-              class="block md:pt-25 pt-20 xl:ml-[12.0625rem] md:ml-[5.625rem] ml-[1.875rem] min-w-[13.4375rem] md:w-[27.8125rem] w-[13.4375rem]"
+              class="block md:mt-[8.4375rem] mt-[5rem] xl:ml-[12.0625rem] md:ml-[5.625rem] ml-[1.875rem] min-w-[13.4375rem] md:w-[27.8125rem] w-[13.4375rem]"
               width="445"
               height="269"
               src="/images/points/point-4.png"
@@ -59,17 +59,17 @@
         </div>
         <div class="number-point-1 shrink-0 max-w-[16.5rem] md:max-w-none md:w-auto w-screen flex">
           <div class="flex flex-col flex-1">
-            <div class="max-w-[26.875rem] min-w-[14rem] pr-5 pb-5 xl:ml-[15.625rem] ml-25">
-              <p class="md:pb-5 pb-2.5 md:text-sm text-[0.625rem]">{ 5 }</p>
-              <p class="pb-2.5 md:text-lg text-sm uppercase font-medium">16 лет</p>
+            <div class="max-w-[26.875rem] min-w-[14rem] pr-5 mb-5 xl:ml-[15.625rem] ml-25">
+              <p class="md:mb-5 mb-2.5 md:text-sm text-[0.625rem]">{ 5 }</p>
+              <p class="mb-2.5 md:text-lg text-sm uppercase font-medium">16 лет</p>
               <p class="font-petrov md:text-[1.25rem] text-[0.75rem]">
                 мы назывались СБК <br class="md:block hidden" />
                 мы меняемся, не изменяя себе!
               </p>
             </div>
-            <div class="max-w-[28.875rem] min-w-[14rem] pr-5 pb-5 mt-auto pl-10">
-              <p class="md:pb-5 pb-2.5 md:text-sm text-[0.625rem]">{ 6 }</p>
-              <p class="pb-2.5 md:text-lg text-sm uppercase font-medium">37 лет</p>
+            <div class="max-w-[28.875rem] min-w-[14rem] pr-5 mb-5 mt-auto">
+              <p class="md:mb-5 mb-2.5 md:text-sm text-[0.625rem]">{ 6 }</p>
+              <p class="mb-2.5 md:text-lg text-sm uppercase font-medium">37 лет</p>
               <p class="font-petrov md:text-[1.25rem] text-[0.75rem]">
                 средний возраст соратников внутри коллектива, <br class="md:block hidden" />
                 все абсолютно молоды душой
@@ -99,9 +99,9 @@
     const words = app.$gsap.utils.toArray(".number-word-1");
     const numbers = app.$gsap.utils.toArray(".number-point-1");
 
-    app.$gsap.to(scrollNumbers.value, {
-      xPercent: -100,
-      ease: "none",
+    app.$gsap.to(numbers, {
+      xPercent: -100 * (numbers.length - 1),
+      ease: "linear",
       scrollTrigger: {
         trigger: scrollNumbers.value,
         pin: true,
@@ -111,8 +111,8 @@
       },
     });
 
-    app.$gsap.to(scrollNumbersWords.value, {
-      xPercent: -120,
+    app.$gsap.to(words, {
+      xPercent: -100 * (words.length - 1),
       ease: "none",
       scrollTrigger: {
         trigger: scrollNumbersWords.value,
